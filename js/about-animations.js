f46caa6938b06f33aca11d53ca1075e30a46791e
Markol17 <markolivierpoulin@hotmail.com>
2018-11-13 01:01:03 -0500

    finished the pricing page
    still need to add functionalities for the presentation tho

---
+++ js/about-animations.js
@@ -0,0 +1,94 @@
+
+
+//Code by Mark-Olvier Poulin
+//This code is for all the animations you see in the home page
+
+$(document).ready(function(){
+var lastScrollTop = 0;
+var up=false;
+
+$('#animate2').hide();
+$('#animate3').hide();
+
+
+window.addEventListener("scroll", function(){
+
+  var st = window.pageYOffset || document.documentElement.scrollTop;
+  var c_arrow= document.getElementById("c-arrow");
+  var	ypos = window.pageYOffset;
+
+    if (st > lastScrollTop){
+        //downscroll
+        c_arrow.style.transform="rotate(45deg)";
+        up=false;
+        console.log(ypos);
+    }
+    else {
+      // upscroll
+      c_arrow.style.transform="rotate(-135deg)";
+      up=true;
+       console.log(ypos);
+     }
+  
+  if (ypos > 550 && ypos < 1050 || ypos>=0 && ypos<63){
+
+      c_arrow.style.borderBottom="2px #000000 solid";
+      c_arrow.style.borderRight="2px #000000 solid";
+
+     }
+     else{
+      c_arrow.style.borderBottom="2px #fff solid";
+      c_arrow.style.borderRight="2px #fff solid";
+
+     }
+ 
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+      $(window).unbind('scroll');
+      c_arrow.style.transform="rotate(-135deg)";
+      c_arrow.setAttribute("href", "#zero");
+      c_arrow.style.borderBottom="2px white solid";
+      c_arrow.style.borderRight="2px white solid";
+      up=true;
+     
+  }
+  if (ypos < 63){
+    c_arrow.style.transform="rotate(45deg)";
+    c_arrow.setAttribute("href", "#first");
+
+    up=false;
+  }
+
+  if(ypos >= 0 && ypos <= 63 && up==false){
+    c_arrow.setAttribute("href", "#first"); 
+  }
+  if(ypos >= 0 && ypos <= 650 && up==true){
+    c_arrow.setAttribute("href", "#zero");
+  }
+  if(ypos >= 63 && ypos <= 538 && up==false){
+    c_arrow.setAttribute("href", "#second");
+  }
+  if(ypos > 650 && ypos <= 900 && up==true){
+    c_arrow.setAttribute("href", "#first");
+  }
+  if(ypos >= 538 && ypos < 950 && up==false){
+    c_arrow.setAttribute("href", "#third");
+  }
+  if(ypos > 900 && ypos < 1000 && up==true){
+    c_arrow.setAttribute("href", "#second");
+  }
+
+  if (st > lastScrollTop || st < lastScrollTop){
+        //scroll detection
+     
+         if(ypos >= 200){
+          $('#animate2').fadeIn();
+        }
+         if(ypos >= 700){
+          $('#animate3').fadeIn();
+        }
+    }
+
+  lastScrollTop = st <= 0 ? 0 : st; // For Mobile
+}, false);
+
+})
--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,14 @@
 //Code by Mark-Olvier Poulin
-//This code is for all the animation you see in the navigation bar
-var lastScrollTop = 0;
+//This code is for all the animation you see in the navigation bar as well as the navigation of the home page
 //var for logo animation
 var time_on=0;
 var time_out=0;
 var count=0;
 var active=false;
+$(document).ready(function(){
+var lastScrollTop = 0;
 //scrolling animations
+
 window.addEventListener("scroll", function(){
 
   var st = window.pageYOffset || document.documentElement.scrollTop;
@@ -21,16 +23,16 @@ window.addEventListener("scroll", function(){
   if (ypos > 450 ){
 
 
-  if (st > lastScrollTop){
-      //downscroll
-      header.style.margin="-70px 0";
-
-    }
-    else {
-      // upscroll
-      header.style.margin="0";
-     }
+    if (st > lastScrollTop){
+        //downscroll
+        header.style.margin="-70px 0";
+      }
+      else {
+        // upscroll
+        header.style.margin="0";
+       }
   }
+  
   if(ypos > 300){
      header.style.backgroundColor="white";
      logo_text.style.color="black";
@@ -40,16 +42,19 @@ window.addEventListener("scroll", function(){
      nav_a3.style.color="black";
 
     }
-    if(ypos <=696){
+  if(ypos <=696){
        header.style.backgroundColor="rgba(0, 0, 0, 0.35)";
        logo_text.style.color="white";
        nav_a0.style.color="white";
        nav_a1.style.color="white";
        nav_a2.style.color="white";
        nav_a3.style.color="white";
-    }
+  }
+
   lastScrollTop = st <= 0 ? 0 : st; // For Mobile
-  }, false);
+}, false);
+
+})
 //logo animation on hover and out
 function animation(){
   if (active==false){
@@ -76,6 +81,34 @@ function back_to_logo(){
   setTimeout(function(){
     logo = document.getElementById('logo');
     logo.src="./img/logo1.png";
+    active=false;
+    }, count)
+}
+function a_animation(){
+  if (active==false){
+    logo = document.getElementById('logo');
+    logo.src="../img/animation.gif";
+    time_on=Date.now();
+    active=true;  
+  }
+}
+function a_back_to_logo(){
+  time_out=Date.now();
+  //keeping track of looping animation
+  count=0;
+  count=3750-(time_out-time_on);
+  if (count>=0){
+    count=3750-(time_out-time_on);
+  }
+  else{
+    diff=(time_out-time_on)
+    reff=Math.round((diff/3750)+0.5);
+    count=(reff*3750)-diff;
+  }
+  //setting timeout witch count
+  setTimeout(function(){
+    logo = document.getElementById('logo');
+    logo.src="../img/logo1.png";
     active=false;
     }, count)
 }
